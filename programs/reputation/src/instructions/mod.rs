699657c75a24a22b4d5108db2bf18fe13c5cc1fd
WritNetwork <257292411+WritNetwork@users.noreply.github.com>
2026-02-25 02:39:23 +0000

    feat: implement recalculate_score instruction

--- programs/reputation/src/instructions/mod.rs
+++ programs/reputation/src/instructions/mod.rs
@@ -1,3 +1,13 @@
 pub mod initialize_reputation;
+pub mod open_dispute;
+pub mod recalculate_score;
+pub mod register_reporter;
+pub mod report_action;
+pub mod resolve_dispute;
 
 pub use initialize_reputation::*;
+pub use open_dispute::*;
+pub use recalculate_score::*;
+pub use register_reporter::*;
+pub use report_action::*;
+pub use resolve_dispute::*;
